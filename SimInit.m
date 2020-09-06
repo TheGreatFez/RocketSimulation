682726ef1a3af177cddb1d8fcfb24e47676fddec
TheGreatFez <raul.maldonado83@gmail.com>
2020-09-06 10:07:18 -0500

    Added Mass, Thrust, and Isp data of the ship

--- SimInit.m
+++ SimInit.m
@@ -12,6 +12,13 @@ SimData.Planet.Radius = 6000000; % m
 
 SimData.Constants.g0 = 9.81; % m/s^2
 
+%% Ship Data
+
+SimData.Ship.DryMass = 1000;    % kg
+SimData.Ship.WetMass = 10000;   % kg
+SimData.Ship.Isp     = 300;     % s
+SimData.Ship.Thrust  = 200;     % kN
+
 %% End Conditions
 
 SimData.TargetOrbit = 200000; % m, altitude of desired parking orbit
